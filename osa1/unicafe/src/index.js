c2b1de178da58947c166b8383d0487d8c9f54103
Saku Myllymäki <saku.myllymaki@aalto.fi>
2019-05-03 20:00:24 +0300

    unicafe valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,38 +7,55 @@ const Button = ({handleClick, name}) => (
 const Header = ({title}) => (
     <h1>{title}</h1>
 )
-const Statistics = ({text, value}) => (
-    <p>{text} {value}</p>
-)
-const App = () => {
-  
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
+const Statistic = ({text, value}) => {
+    return(
+        <tbody>
+            <tr>
+                <td>{text}</td>
+                <td>{value}</td>
+            </tr>
+        </tbody> 
+    )
+}
+const Statistics = ({data}) => {
+    const {bad, neutral, good} = data
     const total = () => (
         good + neutral + bad
     )
-    const showStats = () => {
+
+    const positive = () => good * 100 / total()
+    
+    const average = () => (good - bad) / total()
+
+    const stats = () => {
         if(total() == 0) {
             return <p>Ei yhtään palautetta annettu</p>
         }
         else {
             return(
-            <>
-                <Statistics text={"Hyvä:"} value = {good}></Statistics>
-                <Statistics text={"Neutraali:"} value = {neutral}></Statistics>
-                <Statistics text={"Huono:"} value = {bad}></Statistics>
-                <Statistics text={"Yhteensä:"} value = {total()}></Statistics>
-                <Statistics text={"Keskiarvo:"} value = {average()}></Statistics>
-                <Statistics text={"Positiivisia:"} value = {positive()}></Statistics>
-            </>
+                <table>
+                    <Statistic text="Hyvä:" value = {good}></Statistic>
+                    <Statistic text="Neutraali:" value = {neutral}></Statistic>
+                    <Statistic text="Huono:" value = {bad}></Statistic>
+                    <Statistic text="Yhteensä:" value = {total()}></Statistic>
+                    <Statistic text="Keskiarvo:" value = {average()}></Statistic>
+                    <Statistic text="Positiivisia:" value = {positive()}></Statistic>
+                </table>
             )
         }
     }
-    const positive = () => good * 100 / total()
-    
-    const average = () => (good - bad) / total()
+    return(
+        <div>
+            <Header title={"Statistiikka"}></Header>
+            {stats()}
+       </div>
+    )
+}
+const App = () => {
+  
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
     
     const setNeutralTo = (value) => (
         setNeutral(value)
@@ -49,14 +66,18 @@ const App = () => {
     const setBadTo = (value) => (
         setBad(value)
     )
+    const data = {
+        good: good,
+        neutral: neutral,
+        bad: bad
+    }
     return(
         <div>
             <Header title={"Anna palautetta"}></Header>
             <Button handleClick={() => setGoodTo(good + 1)} name={"Hyvä"}></Button>
             <Button handleClick={() => setNeutralTo(neutral + 1)} name={"Neuraali"}></Button>
             <Button handleClick={() => setBadTo(bad + 1)} name={"Huono"}></Button>
-            <Header title={"Statistiikka"}></Header>
-            {showStats()}
+            <Statistics data={data}></Statistics>
         </div>
     )
 }
