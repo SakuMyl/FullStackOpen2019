d9491e611f5f5f6a91c70ee37512703d0d1c7dbe
Saku Myllymäki <saku.myllymaki@aalto.fi>
2019-05-03 21:55:42 +0300

    anekdootit valmis

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -3,27 +3,54 @@ import ReactDOM from 'react-dom'
 
 const Button = ({handleClick, text}) => {
     return (
-        <div>
-            <button onClick={handleClick}>{text}</button>
-        </div>
+        <button onClick={handleClick}>{text}</button>
     )
 }
 const Header = ({text}) => (
     <h1>{text}</h1>
 )
-const Anecdote = ({text}) => (
-    <p>{text}</p>
-)
+const Anecdote = ({text, votes}) => {
+    return(
+        <div>
+            <p>{text}</p>
+            <p>Has {votes} votes</p>
+        </div>
+    )
+}
 const App = (props) => {
     const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))
 
-    const next = Math.floor(Math.random() * props.anecdotes.length)
-    console.log(next)
+    const select = (index) => {
+        let i = index
+        while(i == selected) i = random()
+        setSelected(i)
+    }
+    const vote = (index) => {
+        const copy = [...votes]
+        copy[index] += 1
+        setVotes(copy)
+    }
+    const mostVoted = () => {
+        let res = 0
+        for(let i = 1; i < props.anecdotes.length; i++) {
+            if(votes[i] > votes[res]) res = i
+        }
+        return res
+    }
+    const best = mostVoted()
+
+    const random = () => Math.floor(Math.random() * votes.length)
     return (
         <div>
             <Header text="Anecdote of the day"></Header>
-            <Button handleClick={() => setSelected(next)} text="Next anecdote"></Button>
-            <Anecdote text={props.anecdotes[selected]}></Anecdote>
+            <Anecdote text={props.anecdotes[selected]} votes={votes[selected]}></Anecdote>
+            <div>
+                <Button handleClick={() => vote(selected)} text="Vote"></Button>
+                <Button handleClick={() => select(random())} text="Next anecdote"></Button>
+            </div>
+            <Header text="Anecdote with most votes"></Header>
+            <Anecdote text={props.anecdotes[best]} votes={votes[best]}></Anecdote>
         </div>
     )
 }
