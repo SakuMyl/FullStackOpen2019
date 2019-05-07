e7302614a1f1f305837e70ff1921cc62ccb2b131
myllyms3 <saku.myllymaki@aalto.fi>
2019-05-07 17:54:03 +0300

    m

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Header = ({name}) => (
   <h1>{name}</h1>
@@ -46,16 +47,19 @@ const Persons = ({persons}) => {
   )
 }
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Martti Tienari', number: '040-123456' },
-    { name: 'Arto Järvinen', number: '040-123456' },
-    { name: 'Lea Kutvonen', number: '040-123456' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [newConstraint, setNewConstraint] = useState('')
 
+  useEffect(() =>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+    }, [])
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
--- testapp/testapp/src/index.js
+++ testapp/testapp/src/index.js
@@ -3,34 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2019-01-10T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2019-01-10T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2019-01-10T19:20:14.298Z',
-    important: true
-  }
-]
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+})
 
 ReactDOM.render(
-  <App notes={notes} />,
+  <App/>,
   document.getElementById('root')
 )
