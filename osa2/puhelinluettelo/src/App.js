63f57bad47cd9d68fe26dd1e3e0efbd375bfe1ad
Saku Myllymäki <saku.myllymaki@aalto.fi>
2019-05-07 10:29:30 +0300

    puhelinluettelo edistyy

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,23 +1,79 @@
 import React, { useState } from 'react'
 
 const Header = ({name}) => (
-  <h2>{name}</h2>
+  <h1>{name}</h1>
 )
+const Filter = (props) => {
+  return (
+    <div>
+      Rajaa näytettäviä: <input
+        value = {props.constraint}
+        onChange = {props.handleChange}
+      />
+    </div>
+  )
+}
+const NewPersonForm = (props) => {
+  return(
+    <div>
+      <h2>Lisää uusi</h2>
+      <form onSubmit = {props.handleSubmit}>
+        <div>
+          nimi: <input 
+            value = {props.name}
+            onChange={props.handleNameChange}
+            />
+        </div>
+        <div>
+          numero: <input
+          value = {props.number}
+          onChange={props.handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+const Persons = ({persons}) => {
+  return(
+    <div>
+      <h2>Numerot</h2>
+      {persons}
+    </div>
+  )
+}
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+  const [newConstraint, setNewConstraint] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-  const handleNewName = (event) => {
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  const handleConstraintChange = (event) => {
+    console.log(event.target.value)
+    setNewConstraint(event.target.value)
+  }
+  const handleNewPerson = (event) => {
     event.preventDefault()
     console.log(persons.concat(newName))
     const person = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if(persons.map(person => person.name).includes(newName)) {
       window.alert(`${newName} on jo luettelossa`)
@@ -25,25 +81,28 @@ const App = () => {
       setPersons(persons.concat(person))
     }
   } 
-  const names = () => (
-    persons.map(person => <p key={person.name}>{person.name}</p>)
+  const joinNameAndNumber = (person) => (
+    `${person.name.toLowerCase()} ${person.number}`
+  )
+  const filterPersons = () => (
+    persons.filter(person => joinNameAndNumber(person).includes(newConstraint.toLowerCase()))
+           .map(person => <p key={person.name}>{person.name} {person.number}</p>)
   )
   return (
     <div>
       <Header name='Puhelinluettelo'></Header>
-      <form onSubmit = {handleNewName}>
-        <div>
-          nimi: <input 
-            value = {newName}
-            onChange={handleNameChange}
-            />
-        </div>
-        <div>
-          <button type="submit">lisää</button>
-        </div>
-      </form>
-      <h2>Numerot</h2>
-      {names()}
+      <Filter
+        constraint={newConstraint}
+        handleChange={handleConstraintChange}
+        ></Filter>
+      <NewPersonForm
+        name={newName}
+        number={newNumber}
+        handleSubmit={handleNewPerson}
+        handleNumberChange={handleNumberChange}
+        handleNameChange={handleNameChange}
+        ></NewPersonForm>
+      <Persons persons={filterPersons()}></Persons>
     </div>
   )
 
