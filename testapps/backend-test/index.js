d361669fbde2f5526ed86e30981043366f4df1ab
Saku Myllymäki <saku.myllymaki@aalto.fi>
2019-05-21 18:50:53 +0300

    merge conflicts resolved

--- testapps/backend-test/index.js
+++ testapps/backend-test/index.js
@@ -3,9 +3,13 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.use(morgan('tiny'))
 app.use(cors())
+
 
 let notes = [
     {
@@ -50,6 +54,32 @@ app.delete('/notes/:id', (request, response) => {
   
     response.status(204).end();
 });
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+app.post('/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId() 
+  }
+  notes = notes.concat(note)
+
+  response.json(note)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
