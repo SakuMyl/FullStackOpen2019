60bb097b28dbc6b6f9b36db0e31a0b840c133418
myllyms3 <saku.myllymaki@aalto.fi>
2019-05-28 22:38:57 +0300

    app refactored

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+//app.use(middleware.unknownEndpoint)
+//app.use(middleware.errorHandler)
+
+module.exports = app
