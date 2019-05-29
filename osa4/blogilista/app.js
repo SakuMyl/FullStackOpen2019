065a3ed6f6014b694f137d9dd8bfcbbe834e83bc
Saku Myllymäki <saku.myllymaki@aalto.fi>
2019-05-29 20:04:14 +0300

    added eslint

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,18 +7,18 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
 
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 
-//app.use(middleware.unknownEndpoint)
-//app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
