d4a6ee4ff4d752920afcca62e04f628787eacba6
myllyms3 <saku.myllymaki@aalto.fi>
2019-06-01 18:17:30 +0300

    refactor get method to use await and async

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,21 +2,21 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs.map(blog => blog.toJSON()))
-    })
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if (blog) {
-                response.json(blog.toJSON())
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+    try {
+        const blog = await Blog.findById(request.params.id)
+        if(blog) {
+            response.json(blog.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    } catch(error) {
+        next(error)
+    }
 })
 
 blogsRouter.post('/', (request, response, next) => {
